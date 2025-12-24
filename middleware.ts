7290bae7025627f26769c4958ffd7818d002dd06
Puvistyne Rajasegar <puvistyne.rajasegar@azeoo.com>
2025-12-24 11:32:51 +0100

    Enhance middleware and update package-lock.json
    - Add a custom middleware function to handle Next.js requests.
    - Update matcher configuration to exclude specific paths for improved routing.
    - Upgrade dependencies in package-lock.json, including @emnapi/runtime, sharp, and next-intl, to their latest versions for better performance and security.
    - Introduce new optional dependencies and update existing ones for compatibility.

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,17 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./src/i18n/routing";
+import { NextRequest, NextResponse } from "next/server";
 
 export default createMiddleware(routing);
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next()
+}
 
 export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
+  // matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
